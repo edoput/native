c98e79b7a79b59a0edf7c3fc8c1f7c77f465022a
Edoardo Putti <edoardo.putti@gmail.com>
2022-10-05 22:23:33 +0200

    add echo server

---
+++ cmd/echo/main.go
@@ -0,0 +1,31 @@
+// echo is an example native end of the
+// native messaging web-extension standard
+//
+// This example server shows how to implement
+// an echo service that echoes whatever message
+// the browser extension sent.
+
+package main
+
+import (
+	"fmt"
+	"io"
+        "os"
+	"github.com/edoput/native"
+)
+
+type Echo struct {}
+
+func (Echo) ServeNative(w native.ResponseWriter, m *native.Message) {
+        fmt.Fprintf(os.Stderr, "Incoming message: %d bytes\n", m.ContentLength)
+        fmt.Fprintln(os.Stderr, "Redirecting message body")
+        io.Copy(w, m.Body)
+}
+
+func main() {
+
+        var server = native.Server{Handler: Echo{}}
+        if err := server.ListenAndServe(); err != nil {
+                fmt.Fprintln(os.Stderr, err)
+        }
+}
